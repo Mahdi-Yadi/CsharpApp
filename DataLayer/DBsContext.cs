7ec4d989a50f55020040b84eafa299a0fdbb579c
Mahdi <137503197+Mahdi-Yadi@users.noreply.github.com>
2024-08-20 12:01:34 +0330

    Add DB

--- DataLayer/DBsContext.cs
+++ DataLayer/DBsContext.cs
@@ -1,5 +1,23 @@
-﻿namespace DataLayer;
-public class DBsContext
+﻿using DataLayer.Entities.Account;
+using Microsoft.EntityFrameworkCore;
+namespace DataLayer;
+public class DBsContext : DbContext
 {
+
+    private static string ConnectionString()
+    {
+        return "Server=.; DataBase=CSharpApp.DB; Integrated Security=True;TrustServerCertificate=true";
+    }
+
+    #region Account
+
+    public DbSet<User> Users { get; set; }
+
+    #endregion
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionString());
+    }
 
 }
---
+++ DataLayer/Entities/Account/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace DataLayer.Entities.Account;
+public class User 
+{
+
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string PhonNumber { get; set; }
+
+    public DateTime CreateDate { get; set; }
+
+}
